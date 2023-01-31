1cff8af70ed6bedc25a0b2d8ca9641b61b6e7eaa
Luciafeliu <feliulucia0@gmail.com>
2023-01-30 21:10:56 -0300

    portfolio final

--- javascript/app.js
+++ javascript/app.js
@@ -1,1 +1,5 @@
-var barraDeProgreso = document.getElementsByClassName("barra-progreso")
+const botonVolver = document.querySelector("#boton-footer");
+
+botonVolver.addEventListener("click", function() {
+    window.scrollTo(0, 0);
+})
